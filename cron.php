63f9a57a54d23148d97b12585a43feb56e59cfbf
Rudie Dirkx <github@hotblocks.nl>
2019-07-17 20:11:54 +0200

    no articles/cookie wall detection

--- cron.php
+++ cron.php
@@ -9,6 +9,11 @@ $client = new Client();
 $links = $client->getArticleLinks();
 
 $total = count($links);
+if ($total == 0) {
+	echo "Can't find articles! Cookie wall? =(\n";
+	exit(1);
+}
+
 $new = 0;
 foreach ($links as $link) {
 	// echo "(" . $link->getCategory() . ") " . $link->getLabel() . "\n" . $link->getUrl() . "\n";
@@ -18,4 +23,4 @@ foreach ($links as $link) {
 	// echo "\n";
 }
 
-echo "$new/$total new\n";
+echo "$new / $total new\n";
