b3701554c54e53499aa61abb3ea995eb10ffda26
Rudie Dirkx <github@hotblocks.nl>
2020-06-13 15:39:25 +0200

    -categories

--- index.php
+++ index.php
@@ -4,12 +4,10 @@ use rdx\ed\Article;
 
 require __DIR__ . '/inc.bootstrap.php';
 
-$hiddenCategories = $db->select_fields('categories', 'category', ['hide' => 1]) ?: ['x'];
 $checkedCategories = $db->select_fields('categories', 'category', ['checked' => 1]) ?: ['x'];
 $newTime = strtotime('-7 days');
 
-$articles = Article::all('category NOT IN (?) AND saved_on > ? ORDER BY id DESC LIMIT 250', [$hiddenCategories, $newTime]);
-$categories = $db->select_fields('articles', 'category', 'category NOT IN (?) GROUP BY category', [$hiddenCategories]);
+$articles = Article::all('category IN (?) AND saved_on > ? ORDER BY id DESC LIMIT 500', [$checkedCategories, $newTime]);
 
 $lastSync = $db->max('articles', 'saved_on');
 
@@ -29,18 +27,11 @@ li {
 }
 </style>
 
-<p class="filters">
-	<? foreach ($categories as $category): ?>
-		<label>
-			<input type="checkbox" value="<?= html($category) ?>" <? if (isset($checkedCategories[$category])): ?>checked<? endif ?> />
-			<?= html($category) ?>
-		</label>
-	<? endforeach ?>
-</p>
+<p><?= count($articles) ?> since <?= date('D Y-m-d H:i', $newTime) ?>:</p>
 
 <ul>
 	<? foreach ($articles as $article): ?>
-		<li data-category="<?= html($article->category) ?>">
+		<li>
 			[<?= date('D H:i', $article->saved_on) ?>]
 			<a href="<?= html($article->url) ?>" rel="noreferrer">
 				[<?= html($article->category) ?>]
@@ -53,15 +44,6 @@ li {
 <p>Laatste sync: <kbd><?= date('Y-m-d H:i', $lastSync) ?></kbd></p>
 
 <p><a href="config.php">Config</a></p>
-
-<script>
-const filter = function() {
-	const shows = [].map.call(document.querySelectorAll('input:checked'), el => el.value);
-	document.querySelectorAll('li[data-category]').forEach(el => el.hidden = shows.indexOf(el.dataset.category) == -1);
-};
-document.querySelector('.filters').addEventListener('change', filter);
-filter();
-</script>
 
 <details>
 	<summary>Queries (<?= count($db->queries) ?>)</summary>
